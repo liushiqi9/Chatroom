ee587341901efbca9313a61fb3d7b82633d067af
liushiqi9 <sla241@sfu.ca>
2015-04-01 19:56:31 -0700

    1.1
    add clear button having trouble with the history query

--- index.js
+++ index.js
@@ -7,16 +7,64 @@ var db = new mongodb.Db('mydb', mongodbServer);
 
 io.on('connection', function(socket){
 	console.log('a user connected');
-  socket.on('chat message', function(user_name,msg){
-  	console.log('message received');
-    io.emit('chat message',user_name,msg);
-    db.open(function() {
-    db.collection('mycoll', function(err, collection) {
-        /* Insert a data */
-        collection.insert({
-            message: msg,
-            username: user_name,
-            time: Date.parse(new Date()),
+	var client_name;
+	var time_start=Date.parse(new Date());
+	 
+    socket.on('user name', function(user_name){
+  		console.log('name received');
+  		client_name=user_name;
+
+    });
+   //   socket.on('history', function(){
+  	// 	db.open(function() {
+		 //     db.collection('mycoll', function(err, collection) {
+		 //        /* Insert a data */
+		 //        console.log('at least database');
+		 //         collection.find({ username: client_name,timestart:{"$gt":1}}, function(err, data) {
+			//             /* Found this People */
+			//             if (data) {
+			//             	console.log('at least data');
+			//                 collection.find({time: {"$gte":data.timestart,"$lte":data.timeend}},{username:1,message:1},function(err,msg){
+			//                 io.emit('chat message',msg.username,msg.message);
+			//                 console.log('got something');
+			//                 console.log(msg.message);
+			//                 });
+			//             } else {
+			//                 console.log('Cannot found');
+			//             }
+			//         });
+		 //       }); 
+		 //    });
+	 	// }); 
+
+
+    socket.on('chat message', function(msg){
+	 			io.emit('chat message',client_name,msg);
+	 		 db.open(function() {
+		     db.collection('mycoll', function(err, collection) {
+		        /* Insert a data */
+		        collection.insert({
+		            message: msg,
+		            username: client_name,
+		            time: Date.parse(new Date()),
+		        }, function(err, data) {
+		            if (data) {
+		                console.log('Successfully Insert');
+		            } else {
+		                console.log('Failed to Insert');
+		            }
+		        });
+		       }); 
+		    });
+	 	}); 
+
+  socket.on('disconnect',function(){
+  	console.log('user disconnect');
+  	 db.collection('mycoll', function(err, collection) {
+  	collection.insert({
+            username: client_name,
+            timestart: time_start,
+            timeend: Date.parse(new Date()),
         }, function(err, data) {
             if (data) {
                 console.log('Successfully Insert');
@@ -24,12 +72,7 @@ io.on('connection', function(socket){
                 console.log('Failed to Insert');
             }
         });
-       }); 
-    });
-
-  });
-  socket.on('disconnect',function(){
-  	console.log('user disconnect');
+  	 });
   });
 
 });
